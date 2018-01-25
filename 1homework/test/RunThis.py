eef1bfa64b3976e874b914a7d74054446fae21ff
Ian <ian.taulli@ttu.edu>
2018-01-25 12:03:22 -0600

    clean up

--- 1homework/test/RunThis.py
+++ 1homework/test/RunThis.py
@@ -3,38 +3,42 @@ Created: Wed Jan 24 13:49:50 2018
 Author: Ian Taulli
 Description: takes the data from xy.txt and makes a plot
 """
-
 import matplotlib.pyplot as plt
 
+#initailize some lists and open the file
 temp = []
+number = []
 xlist = []
 ylist = []
 file = open('xy.txt')
 
 for line in file:
     
+    #skip lines that are comments or empty space
     if line.startswith(('#',' ')):
         continue
     
+    #turn all the commas into spaces
     table = str.maketrans(',',' ')
     line = line.translate(table)
     
+    #split the x and y values and stack them
     x = line.split(maxsplit=1)
     temp += x
 
-for i in range(len(temp)):
+for entry in temp:
     
-    entry = temp[i]
+    #turn each entry into a float object
     entry = entry.strip('\n')
-    number = float(entry)
-    
-    if i%2 == 0:
-        xlist += [number]
-    
-    if i%2 == 1:
-        ylist += [number]
+    entry = float(entry)
+    number.append(entry)
 
+#split the x and y values into two lists
+xlist = number[0::2]
+ylist = number[1::2]
+
+#plot the lists
 plt.plot(xlist,ylist)
 ax = plt.gca()
-ax.set_title('fig. 1 (Taulli)')
+ax.set_title('HW1 (Taulli)')
 plt.savefig('fig1.pdf')
