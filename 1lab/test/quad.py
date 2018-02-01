2b71ff2b5269c469b0ee6db8b6ff5333773a001e
Ian <ian.taulli@ttu.edu>
2018-01-31 21:09:39 -0600

    quad fix

--- 1lab/test/quad.py
+++ 1lab/test/quad.py
@@ -6,13 +6,15 @@ Description: creates a particular instance of qsolve
 import numpy as np
 import qsolve as q
 
-# the coefficents for (x-(10**10)*(x-(10**-10)
-a = 1.0
-b = -(10.0**20+1)/(10**10)
+# the coefficents
+a = 1
+b = -10.0**20
 c = 1
 
+#calls the solution functions
 ans1,ans2 = q.standard(a,b,c)
 ans3,ans4 = q.modified(a,b,c)
 
-print("results for standard: ",ans1," and ",ans2)
-print("results for modified: ",ans3," and ",ans4)
+#creates the output
+print('standard quadriatic formula: {:.2f} and {:.2f}'.format(ans1,ans2))
+print('modified algorithm: {:.2f} and {:.2f}'.format(ans3,ans4))
