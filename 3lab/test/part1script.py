87d1919b4f47b82b1cd9d0a5205521df454f19e1
Ian <ian.taulli@ttu.edu>
2018-02-13 15:23:20 -0600

    feb 13 class

--- 3lab/test/part1script.py
+++ 3lab/test/part1script.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import derivatives as d
 
 #define the functions
-def exponent(x):
+def exp(x):
     return np.exp(x)
 def log(x):
     return np.log(x+1)
@@ -18,7 +18,8 @@ def sin(x):
 
 #define the data dictionary
 fun_points = np.linspace(0,2*np.pi,100)
-level2 = {'1true','1forward', '1symmetric', '2true', '2approx'}
+init = np.zeros(len(fun_points))
+level2 = {'1true':init,'1forward':init, '1symmetric':init, '2true':init, '2approx':init}
 data = {'exp': level2, 'log': level2, 'sin': level2}
 
 #set up the data arrays and save them for a given step size
@@ -30,13 +31,23 @@ data['sin']['1true'] = 2*np.cos(2*fun_points)
 data['sin']['2true'] = -4*np.sin(2*fun_points)
 
 def data_fill(step_size):
-    data['exp']['1forward'] = d.FirstForward(exponent,fun_points,step_size)
-    data['exp']['1symmetric'] = d.FirstSymmetric(exponent,fun_points,step_size)
-    data['exp']['2approx'] = d.Second3Point(exponent,fun_points,step_size)
-    data['log']['1forward'] = d.FirstForward(log,fun_points,step_size)
-    data['log']['1symmetric'] = d.FirstSymmetric(log,fun_points,step_size)
-    data['log']['2approx'] = d.Second3Point(log,fun_points,step_size)
-    data['sin']['1forward'] = d.FirstForward(sin,fun_points,step_size)
-    data['sin']['1symmetric'] = d.FirstSymmetric(sin,fun_points,step_size)
-    data['sin']['2approx'] = d.Second3Point(sin,fun_points,step_size)
+    call = {'exp':exp, 'log':log, 'sin':sin}
+    for key in data:
+        data[key]['1forward'] = d.FirstForward(call[key],fun_points,step_size)
+        data[key]['1symmetric'] = d.FirstSymmetric(call[key],fun_points,step_size)
+        data[key]['2approx'] = d.Second3Point(call[key],fun_points,step_size)
     return 'done'
+
+#start evaluating median errors for given step sizes
+number_evals = 100
+init2 = np.zeros(number_evals)
+level3 = {'1true':init2,'1forward':init2, '1symmetric':init2, '2true':init2, '2approx':init2}
+error = {'exp': level2, 'log': level2, 'sin': level2}
+names1('exp','log','sin')
+for i, size in enumerate(np.linspace(1e-14,0.1,number_evals)):
+    data_fill(size)    
+    for key1, key2 in data.items():
+        if key2 == '2approx':
+            error[key1][key2][i] = np.median(d.RelativeError(data[key1][key2],data[key1]['2true']))
+        else:
+            error[key1][key2][i] = np.median(d.RelativeError(data[key1][key2],data[key1]['1true']))
