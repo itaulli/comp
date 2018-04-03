ffd5cd0c7589397326d59b6e0e1aa9a27d7c0c04
Ian <ian.taulli@ttu.edu>
2018-04-03 10:32:54 -0500

    asa

--- 8lab/test/SiteSampler_example.py
+++ 8lab/test/SiteSampler_example.py
@@ -9,8 +9,11 @@ __author__="Igor Volobouev (i.volobouev@ttu.edu)"
 __version__="0.4"
 __date__ ="Mar 22 2018"
 
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import dla
+import numpy as np
 
 # Example array of transition probabilities (not normalized)
 p = ((0, 0, 1, 2, 1, 0, 0),
@@ -20,7 +23,7 @@ p = ((0, 0, 1, 2, 1, 0, 0),
      (1, 2, 3, 4, 3, 2, 1),
      (0, 1, 2, 3, 2, 1, 0),
      (0, 0, 1, 2, 1, 0, 0))
-a = array(p, 'float')
+a = np.array(p, 'float')
 
 # ny is the number of rows and nx is the number of columns
 ny, nx = a.shape
@@ -37,26 +40,29 @@ ycoords = [y,]
 gen = dla.CPP11Random()
 
 nsteps = np.arange(100,1100,100)
+mean_dist = []
+mean_vecx = []
+mean_vecy = []
 
 # Perform a random walk. Operator "//" is used to perform integer division.
-for n in nsteps
+for n in nsteps:
     dist_list = []
-    mean_vecx = []
-    mean_vecy = []
     for i in range(n):
         Walker.step(gen())
         xcoords.append(Walker.getI())
         ycoords.append(Walker.getJ())
     
-    for i in range(len(xcoords))
+    for i in range(len(xcoords)):
         dist = np.sqrt(xcoords[i]**2+ycoords[i]**2)
         dist_list.append(dist)
+    
+    mean_dist.append(sum(dist_list)/len(dist_list))
 
     mean_vecx.append(sum(xcoords)/len(xcoords))
     mean_vecy.append(sum(ycoords)/len(ycoords))
 
 plt.figure()
-plt.plot(nsteps,dist_list)
+plt.plot(nsteps,mean_dist)
 ax = plt.gca()
 ax.set_title('mean distance')
 ax.set_xlabel('number of steps')
